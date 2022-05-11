3ffc2024349d23014952f71d364da3c6956122d6
ArtemGonchukov <bulkivtulki@gmail.com>
2022-05-11 23:40:27 +0300

    mod_gulp

--- gulpfile.js
+++ gulpfile.js
@@ -48,33 +48,37 @@ gulp.task('html', function() {
     return gulp.src("src/*.html")
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist/'))
-        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('scripts', function() {
     return gulp.src("src/js/**/*.js")
         .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*")
         .pipe(gulp.dest('dist/fonts'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('icons', function() {
     return gulp.src("src/icons/**/*")
         .pipe(gulp.dest('dist/icons'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('mailer', function() {
     return gulp.src("src/mailer/**/*")
         .pipe(gulp.dest('dist/mailer'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('images', function() {
     return gulp.src("src/img/**/*")
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 
